134a02e41075edfc71122be92a9a06b190c95fde
sambmillennium <sbmusic1992@hotmail.co.uk>
2017-01-09 13:19:23 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='speedthing.py',
-    version='0.1',
-    packages=['paramiko', 'easygui'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
